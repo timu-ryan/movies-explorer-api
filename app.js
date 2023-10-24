08eb65573656ff005c5c91555f19421ebe0af68b
Timur Gafarov <timu.ryan@yandex.ru>
2023-10-24 18:39:43 +0300

    chenged cors

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
-// const cors = require('cors');
+const cors = require('cors');
 const routes = require('./routes/index');
 const { errorCatchMiddleware } = require('./middlewares/catchErrors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
@@ -13,14 +13,6 @@ const { limiter } = require('./middlewares/limiter');
 
 const { PORT = 3000, NODE_ENV, DB_ADDRESS } = process.env;
 
-const allowedCors = [
-  'https://praktikum.tk',
-  'http://praktikum.tk',
-  'localhost:3000',
-  'https://api.timuryanst.nomoredomainsrocks.ru',
-  'https://timuryanst.nomoredomainsrocks.ru',
-];
-
 mongoose.connect(NODE_ENV === 'production' ? DB_ADDRESS : 'mongodb://127.0.0.1:27017/bitfilmsdb', {
   useNewUrlParser: true,
 }).then(() => {
@@ -29,29 +21,7 @@ mongoose.connect(NODE_ENV === 'production' ? DB_ADDRESS : 'mongodb://127.0.0.1:2
 
 const app = express();
 app.use(helmet());
-app.use((req, res, next) => {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  // проверяем, что источник запроса есть среди разрешённых
-  if (allowedCors.includes(origin)) {
-    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Origin', '*');
-  }
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-
-  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers'];
-  if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    // разрешаем кросс-доменные запросы с этими заголовками
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    // завершаем обработку запроса и возвращаем результат клиенту
-    return res.end();
-  }
-  next();
-});
+app.use(cors());
 app.use(limiter);
 app.use(bodyParser.json());
 app.use(requestLogger);
