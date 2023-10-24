394ba8c886f017e524e73be2d5caf785d824bf49
Timur Gafarov <timu.ryan@yandex.ru>
2023-10-24 19:24:20 +0300

    added cross domain func

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ const { limiter } = require('./middlewares/limiter');
 // const { authMiddleware } = require('./middlewares/authMid');
 
 const { PORT = 3000, NODE_ENV, DB_ADDRESS } = process.env;
+const allowedCors = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'localhost:3000',
+  'https://api.timuryanst.nomoredomainsrocks.ru',
+  'https://timuryanst.nomoredomainsrocks.ru',
+];
 
 mongoose.connect(NODE_ENV === 'production' ? DB_ADDRESS : 'mongodb://127.0.0.1:27017/bitfilmsdb', {
   useNewUrlParser: true,
@@ -20,6 +27,30 @@ mongoose.connect(NODE_ENV === 'production' ? DB_ADDRESS : 'mongodb://127.0.0.1:2
 });
 
 const app = express();
+
+app.use((req, res, next) => {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
+  if (allowedCors.includes(origin)) {
+    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Origin', '*');
+  }
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+
+  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    // разрешаем кросс-доменные запросы с этими заголовками
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    // завершаем обработку запроса и возвращаем результат клиенту
+    return res.end();
+  }
+  next();
+});
 app.use(helmet());
 app.use(cors());
 app.use(limiter);
