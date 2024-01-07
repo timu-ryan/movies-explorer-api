6000ac57b99700045136bb154940be885fd03734
Timur Gafarov <timu.ryan@yandex.ru>
2024-01-07 23:04:37 +0300

    deleted required fields in movie schema

--- models/Movie.js
+++ models/Movie.js
@@ -5,26 +5,26 @@ const {
 } = require('../errors/error-texts');
 
 const movieSchema = new mongoose.Schema({
-  country: {
-    type: String,
-    required: true,
-  },
-  director: {
-    type: String,
-    required: true,
-  },
-  duration: {
-    type: Number,
-    required: true,
-  },
-  year: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
+  // country: {
+  //   type: String,
+  //   required: true,
+  // }, // checked
+  // director: {
+  //   type: String,
+  //   required: true,
+  // }, // checked
+  // duration: {
+  //   type: Number,
+  //   required: true,
+  // }, // checked
+  // year: {
+  //   type: String,
+  //   required: true,
+  // }, // checked
+  // description: {
+  //   type: String,
+  //   required: true,
+  // }, // checked
   image: {
     type: String,
     validate: {
@@ -32,7 +32,7 @@ const movieSchema = new mongoose.Schema({
       message: urlValidationFailed,
     },
     required: true,
-  },
+  }, // checked but idk
   trailerLink: {
     type: String,
     validate: {
@@ -40,32 +40,32 @@ const movieSchema = new mongoose.Schema({
       message: urlValidationFailed,
     },
     required: true,
-  },
-  thumbnail: {
-    type: String,
-    validate: {
-      validator: (url) => validator.isURL(url),
-      message: urlValidationFailed,
-    },
-    required: true,
-  },
+  }, // checked same
+  // thumbnail: {
+  //   type: String,
+  //   validate: {
+  //     validator: (url) => validator.isURL(url),
+  //     message: urlValidationFailed,
+  //   },
+  //   required: true,
+  // }, // checked same
   owner: {
     type: mongoose.SchemaTypes.ObjectId,
     ref: 'user',
     required: true,
-  },
-  movieId: {
-    type: Number,
-    required: true,
-  },
+  }, // we don't need to add it in frontend
+  // movieId: {
+  //   type: Number,
+  //   required: true,
+  // }, // checked
   nameRU: {
     type: String,
     required: true,
-  },
-  nameEN: {
-    type: String,
-    required: true,
-  },
+  }, // checked
+  // nameEN: {
+  //   type: String,
+  //   required: true,
+  // }, // checked
 });
 
 module.exports = mongoose.model('movie', movieSchema);
